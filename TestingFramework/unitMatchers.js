a9c01360a9b1fa423f04affb819e820fbc81456f
Ellie Keen <elliekeen95@gmail.com>
2018-05-29 20:12:07 +0100

    Update Testing Framework

--- TestingFramework/unitMatchers.js
+++ TestingFramework/unitMatchers.js
@@ -1,34 +1,43 @@
-var assert = {
-  toBeTrue : function(object) {
-    if (object === true) {
-      console.log('%c Pass! ', 'background: #222; color: #bada55');
-    } else {
-      console.log('%c You shall not pass! ', 'background: #222; color: orange');
-    }
-  },
+(function(exports) {
 
-  toBeFalse : function(object) {
-    if (object === false) {
-      console.log('%c Pass! ', 'background: #222; color: #bada55');
-    } else {
-      console.log('%c You shall not pass! ', 'background: #222; color: orange');
-    }
-  },
+  var passTest = '%c Pass! '
+  var green = 'background: #222; color: #bada55';
+  var failTest = '%c You shall not pass! '
+  var orange = 'background: #222; color: orange';
 
-  toEqual : function(object1, object2) {
-    if (JSON.stringify.object1 === JSON.stringify.object2) {
-      console.log('%c Pass! ', 'background: #222; color: #bada55');
-    } else {
-      console.log('%c You shall not pass! ', 'background: #222; color: orange');
-    }
-  },
+  var assert = {
+    toBeTrue : function(object) {
+      if (object === true) {
+        console.log(passTest, green);
+      } else {
+        console.log(failTest, orange);
+      }
+    },
 
-  toInclude : function(object1, object2) {
-    if (object1.includes(object2)){
-      console.log('%c Pass! ', 'background: #222; color: #bada55');
-    } else {
-      console.log('%c You shall not pass! ', 'background: #222; color: orange');
-    }
-  }
+    toBeFalse : function(object) {
+      if (object === false) {
+        console.log(passTest, green);
+      } else {
+        console.log(failTest, orange);
+      }
+    },
 
-};
+    toEqual : function(object1, object2) {
+      if (JSON.stringify(object1) === JSON.stringify(object2)) {
+        console.log(passTest, green);
+      } else {
+        console.log(failTest, orange);
+      }
+    },
+
+    toInclude : function(object1, object2) {
+      if (object1.includes(object2)){
+        console.log(passTest, green);
+      } else {
+        console.log(failTest, orange);
+      }
+    }
+  };
+
+  exports.assert = assert;
+})(this);
--- spec/unit/sampleUnitTest.js
+++ spec/unit/sampleUnitTest.js
@@ -13,12 +13,12 @@ describe('Sample tests that compare the equality of two objects');
   it('should be equal');
     assert.toEqual('hello', 'hello');
 
-
   it('should be equal');
     assert.toEqual( {'hello': 1 }, {'hello': 1 });
 
   it('should be equal');
     assert.toEqual([3, 5, 6], [3, 5, 6]);
+
 
 describe('Sample tests that include same elements');
 
