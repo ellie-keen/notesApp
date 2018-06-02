e5acfbadfe5f6b99ebef27392207da59e6f17a56
Ellie Keen <elliekeen95@gmail.com>
2018-06-02 18:15:31 +0100

    Remove incomplete constructor

--- spec/unit/singleNoteViewSpec.js
+++
@@ -1,5 +0,0 @@
-describe('SingleNoteView');
-  it('should take a notel model as a parameter');
-    note = new Note('Favourite drink: seltzer');
-    singleNoteView = new SingleNoteView(note);
-    assert.toEqual(singleNoteView, new SingleNoteView(note));
